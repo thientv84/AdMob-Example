4ea34c9f9978620fb635cafc3867d23bfc056674
Johannes Plunien <jplunien@ebay.com>
2015-10-07 07:08:10 +0200

    Init

---
+++ AdMob/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdMob
+//
+//  Created by Plunien, Johannes on 07/10/15.
+//  Copyright © 2015 eBay Kleinanzeigen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdMob/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdMob
+//
+//  Created by Plunien, Johannes on 07/10/15.
+//  Copyright © 2015 eBay Kleinanzeigen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
